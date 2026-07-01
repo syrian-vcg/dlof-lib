33bc87f7b788e093c8a71042cef28fad916ca800
homy <leo740143@gmail.com>
2026-07-02 00:23:12 +0300

    Initial commit from ZIP upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,4 +41,14 @@ publishing {
             from(components["java"])
         }
     }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY")}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
